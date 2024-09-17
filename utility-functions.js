f81beed0d0d9ca6b93a70105393b757fdee90f6a
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2024-09-16 22:56:32 -0500

    Updated card text and added alerts to Portfolio and Resume

--- utility-functions.js
+++ utility-functions.js
@@ -1,10 +1,10 @@
 function bootstrapCard(card) {  
-   const cardString = `<div class="card">
+   const cardString = `<div class="card m-4 p-4">
                             <img class="card-img-top" 
                                 src="${card.src}"
                                 alt="${card.title}"/>
                              <div class="card-body">
-                                <h3 class="card-title">${card.description}</h3>
+                                <h6 class="card-title">${card.description}</h6>
                             </div>
                         </div>`;
     return cardString;
