08f486f78eeba6bfd31ccb340f3afc254839b3bc
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2024-10-25 15:26:10 -0500

    Added new form for scheduling consultations

--- forms.js
+++ forms.js
@@ -1,10 +1,21 @@
-function processForm(event) {
-    document.write("Form Processed");
-    }
 
- function processForm(event) {
-        const form = event.target;
-        const input = form[0];
-            const value = input.value;
-        output( form );
-     }git 
+ /* function myProcessForm(event) {
+    document.write("Form Processed");
+}*/
+
+ //OVERIDES BROWSER DEFAULT BEHAVIOR OF RESETTING THE FORM
+ function processForm(event) { 
+  
+    const form = event.target;
+    const input = form[0];
+        const value = input.value;
+    output(displayMessage);
+ }
+
+ //PROCESS THE FORM
+ const emailSubmitMessage = "<br>" + "<h5>Your message has been sent.</h5>" + "<br>";
+ function displayMessage(message) {
+  window.outputTag.innerHTML = message;
+ }
+
+
