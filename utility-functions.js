3b34b867425c183debc4cb892c7bc21e41b691c6
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2024-10-30 17:37:57 -0500

    Fixing broken stuff

--- utility-functions.js
+++ utility-functions.js
@@ -1,5 +1,5 @@
 //AUTOMATICALLY CREATE BOOTSTRAPPED CARDS ON THE PORTFOLIO PAGE
-function bootstrapCard(card) {  
+function bootstrapPortfolioCard(card) {  
    //declare cardString variable that outputs HTML with Bootstrap CSS and pulls card data from an array
    const cardString = `<div class="card m-4 p-4 bg-dark text-light">
                             <img class="card-img-top" 
@@ -14,5 +14,15 @@ function bootstrapCard(card) {
     return cardString;
  } 
 
-
-//
+ function bootstrapCard(card) {  
+   //declare cardString variable that outputs HTML with Bootstrap CSS and pulls card data from an array
+   const cardString = `<div class="card m-4 p-4 bg-dark text-light">
+                            <img class="card-img-top" 
+                                id="${card.id}"
+                             <div class="card-body">
+                                <h5 class="card-title p-1">${card.title}</h5>
+                                <p class="card-text p-1">${card.description}</p>
+                            </div>
+                        </div>`;
+    return cardString;
+ } 
