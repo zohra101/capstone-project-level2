7a38fde8efc557ffb18a4d497239c189be0cf302
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2024-11-06 21:21:37 -0600

    Added tel validation & server sim for forms

--- forms.js
+++ forms.js
@@ -1,8 +1,4 @@
-/* function myProcessForm(event) {
-    document.write("Form Processed");
-}*/
-
- //OVERIDES BROWSER DEFAULT BEHAVIOR OF RESETTING THE FORM
+] //OVERIDES BROWSER DEFAULT BEHAVIOR OF RESETTING THE FORM
  function processForm(event) { 
     const form = event.target;
     const input = form[0];
@@ -13,6 +9,20 @@
  //PROCESS THE FORM
  const emailSubmitMessage = "<br>" + "<h5>Your message has been sent.</h5>" + "<br>";
  const scheduleSubmitMessage = "<br>" + "<h5>Your consultation request has been submitted. Please check for an email confirmation in th enext 24 hours.</h5>" + "<br>";
- function displayMessage(message) {
-  window.outputTag.innerHTML = message;
- }
+ 
+//  function displayMessage(message) {
+//   window.outputTag.innerHTML = message;
+//  }
+
+ //SERVER SIMULATION FOR FORMS
+ function submitForm(event) {
+    const inputs = event.target;
+    const emailInput = inputs[0];
+    const email = emailInput.value;
+    display("Submitting for " + email + " ...");
+    setTimeout(serverResponse, 5000);
+}
+
+function serverResponse(message) {
+    window.outputTag.innerHTML = message;
+}                    
