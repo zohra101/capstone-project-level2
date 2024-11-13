f50f9e1c5ce392b78ffb25ca010ecdea638a063e
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2024-11-13 17:35:25 -0600

    Was working now broken

--- forms.js
+++ forms.js
@@ -1,28 +1,25 @@
 //OVERIDES BROWSER DEFAULT BEHAVIOR OF RESETTING THE FORM
- function processForm(event) { 
-    const form = event.target;
-    const input = form[0];
-    const value = input.value;
-    output(displayMessage);
- }
+//  function processForm(event) { 
+//     const form = event.target;
+//     const input = form[0];
+//     const value = input.value;
+//     output(displayMessage);
+//  }
 
  //PROCESS THE FORM
- const emailSubmitMessage = "<br>" + "<h5>Your message has been sent.</h5>" + "<br>";
- const scheduleSubmitMessage = "<br>" + "<h5>Your consultation request has been submitted. Please check for an email confirmation in th enext 24 hours.</h5>" + "<br>";
+//  const emailSubmitMessage = "<br>" + "<h5>Your message has been sent.</h5>" + "<br>";
+//  const scheduleSubmitMessage = "<br>" + "<h5>Your consultation request has been submitted. Please check for an email confirmation in th enext 24 hours.</h5>" + "<br>";
  
 //  function displayMessage(message) {
 //   window.outputTag.innerHTML = message;
 //  }
 
- //SERVER SIMULATION FOR FORMS
- function submitForm(event) {
+ //Simulate server processing
+ function handleForm(event) {
+    debugger;
     const inputs = event.target;
-    const emailInput = inputs[0];
+    const emailInput = inputs[3];
     const email = emailInput.value;
     display("Submitting for " + email + " ...");
     setTimeout(serverResponse, 5000);
 }
-
-function serverResponse(message) {
-    window.outputTag.innerHTML = message;
-}                    
