42e9c578a5aa5aa5bfba4af71e0e478756425e2f
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2024-11-13 18:01:02 -0600

    Server simulation broken

--- forms.js
+++ forms.js
@@ -1,25 +1,38 @@
 //OVERIDES BROWSER DEFAULT BEHAVIOR OF RESETTING THE FORM
-//  function processForm(event) { 
-//     const form = event.target;
-//     const input = form[0];
-//     const value = input.value;
-//     output(displayMessage);
-//  }
+ function processForm(event) { 
+    const form = event.target;
+    const input = form[0];
+    const value = input.value;
+    output(displayMessage);
+ }
 
  //PROCESS THE FORM
-//  const emailSubmitMessage = "<br>" + "<h5>Your message has been sent.</h5>" + "<br>";
-//  const scheduleSubmitMessage = "<br>" + "<h5>Your consultation request has been submitted. Please check for an email confirmation in th enext 24 hours.</h5>" + "<br>";
+ const emailSubmitMessage = "<br>" + "<h5>Your message has been sent.</h5>" + "<br>";
+ const scheduleSubmitMessage = "<br>" + "<h5>Your consultation request has been submitted. Please check for an email confirmation in th enext 24 hours.</h5>" + "<br>";
  
 //  function displayMessage(message) {
 //   window.outputTag.innerHTML = message;
 //  }
 
- //Simulate server processing
+ //Simulate form processing
  function handleForm(event) {
-    debugger;
     const inputs = event.target;
     const emailInput = inputs[3];
     const email = emailInput.value;
     display("Submitting for " + email + " ...");
     setTimeout(serverResponse, 5000);
+}
+
+function serverResponse(message) {
+    window.outputTag.innerHTML = message;
+}         
+
+function serverResponsePromise() {
+    const promise = new Promise(serverResolve);
+     promise.then(getMessage);
+     display(emailSubmitMessage);
+}
+
+function getMessage(resolveValue) {
+    display(resolveValue);
 }
